a98534e5668b1bcbd0ddb156e58f8ddfe31340e1
aleempasha2772 <aleempasha1410@gmail.com>
2025-02-10 23:35:38 +0530

    Project Setup

---
+++ src/main/java/com/example/TravelAi/TravelAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.TravelAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TravelAi/TravelAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TravelAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
